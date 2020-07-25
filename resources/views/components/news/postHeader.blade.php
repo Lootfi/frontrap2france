c8f16f7e44393e7a0298d98918416eb92214ecb1
Karam Henni <karam.tenes@gmail.com>
2020-07-25 13:28:31 +0100

    Improvements

--- resources/views/components/news/postHeader.blade.php
+++ resources/views/components/news/postHeader.blade.php
@@ -1,8 +1,10 @@
-<section class="banner_01" style="background:url('{{$article->Avatar}}') no-repeat center center / cover;">
-            <div class="container">
+<section class="banner_01" style="position:relative; background:url('{{$article->Avatar}}') no-repeat center center / cover;">
+    <div style="position: absolute; width: 100%; height: 100%; top: 0;left: 0;right: 0;bottom: 0;background-color: rgba(0,0,0,0.5);"></div>
+            <div class="container" style="z-index: 3;">
                 <div class="row">
                     <div class="col-lg-12 text-center">
-                        <div class="banner_1_content">
+                        <div class="banner_1_content relative">
+                            
                             <a href="{{Route('categories.show',$article->Category->slug)}}" class="cats blues2">{{$article->Category->nom}}</a>
                             <h2 class="title" tag="{{$article->tag}}">{{$article->titre}}</h2>
                             <div class="post_banner_meta clearfix">
--- resources/views/pages/news/show.blade.php
+++ resources/views/pages/news/show.blade.php
@@ -108,7 +108,8 @@
             const template = 
             
             `
-            <section class="banner_01" style="background:url('${article.Avatar}') no-repeat center center / cover;">
+            <section class="banner_01" style="position:relative; background:url('${article.Avatar}') no-repeat center center / cover;">
+            <div style="position: absolute; width: 100%; height: 100%; top: 0;left: 0;right: 0;bottom: 0;background-color: rgba(0,0,0,0.5);"></div>
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12 text-center">
