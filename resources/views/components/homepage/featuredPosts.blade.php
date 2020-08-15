4bb53c8bf8599e917a875aeeb15701f7233501a0
Karam Henni <karam.tenes@gmail.com>
2020-08-15 12:11:56 +0100

    fix issue

--- resources/views/components/homepage/featuredPosts.blade.php
+++ resources/views/components/homepage/featuredPosts.blade.php
@@ -8,7 +8,7 @@
                                 <div class="fi1c_categories">
                                     <a href="{{Route('categories.show',$featuredArticles->first()->Category->slug)}}" class="cats blues2">{{$featuredArticles->first()->Category->nom}}</a>
                                 </div>
-                                <h2><a href="{{Route('news.show',$featuredArticles->first()->tag)}}">{{$featuredArticles->first()->titre}}</a></h2>
+                                <h2><a href="/news/{{$featuredArticles->first()->tag)}}">{{$featuredArticles->first()->titre}}</a></h2>
                                 <div class="fi1c_meta clearfix">
                                     <span class="fi1cm_author">By <a href="{{Route('editors.show',$featuredArticles->first()->Creator->slug)}}">{{$featuredArticles->first()->Creator->Full_Name}}</a></span>
                                     <span class="fi1cm_date font-italic"><a href="#">{{$featuredArticles->first()->DateActu}}</a></span>
@@ -24,7 +24,7 @@
                                         <div class="fi1c_categories">
                                             <a href="{{Route('categories.show',$article->Category->slug)}}" class="cats blues2">{{$article->Category->nom}}</a>
                                         </div>
-                                        <h2><a href="{{Route('news.show',$article->tag)}}">{{$article->titre}}</a></h2>
+                                        <h2><a href="/news/{{$article->tag}}">{{$article->titre}}</a></h2>
                                         <div class="fi1c_meta clearfix">
                                             <span class="fi1cm_author">By <a href="{{Route('editors.show',$article->Creator->slug)}}">{{$article->Creator->Full_Name}}</a></span>
                                             <span class="fi1cm_date font-italic"><a href="#">{{$article->DateActu}}</a></span>
