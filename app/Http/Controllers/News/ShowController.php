540d62e441eaee10a95e0a392d646b1b1c32ff52
Karam Henni <karam.tenes@gmail.com>
2020-08-17 21:54:05 +0100

    testing

--- app/Http/Controllers/News/ShowController.php
+++ app/Http/Controllers/News/ShowController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\News;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request; 
 use App\Models\Category;
+use DB;
 
 use App\Models\Article;
 class ShowController extends Controller
@@ -16,8 +17,14 @@ class ShowController extends Controller
 
     	if($article = Article::fetchByTag($tag)){
     		
+            $article = DB::table('r2f_new_actualite_testing_copy AS articles')->select('*')
+                       ->where('articles.tag',$tag)
+                       ->get();
+
+            return $article;
+
             return Article::fetchByTag($tag);
-            
+
     		return view('pages.news.show',[
 
     			'article' => $article,	
