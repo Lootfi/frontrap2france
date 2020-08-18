03f3b5217b00c947f6bb65bee7368abf1a2934b6
Karam Henni <karam.tenes@gmail.com>
2020-08-18 13:13:47 +0100

    fix issue

--- app/Http/Controllers/Api/Categories/IndexController.php
+++ app/Http/Controllers/Api/Categories/IndexController.php
@@ -35,7 +35,7 @@ class IndexController extends Controller
                 $articles = DB::table('r2f_new_actualite_testing_copy AS articles')
                             ->join('r2f_new_actualite-categorie AS categorie','articles.idcat','=','categorie.id')
                             ->join('r2f_new_adminstrators AS creator','articles.admin_creator_id','=','creator.id')
-                            ->select('articles.id','articles.titre','articles.image','articles.updated_at','articles.tag','articles.created_at','articles.status','categorie.nom as Category','creator.full_name as CreatorFullName','creator.email as CreatorEmail', 'articles.type')
+                            ->select('articles.id','articles.titre','articles.image','articles.updated_at','articles.tag','articles.created_at','articles.status','categorie.nom as Category','creator.full_name as CreatorFullName','creator.slug as CreatorSlug', 'articles.type')
                              ->where('idcat',$category->id)
                              ->orderBy('articles.created_at','DESC')
                                 ->take(4)
@@ -52,7 +52,9 @@ class IndexController extends Controller
             $item->image = "https://cd1.rap2france.com/public/medias/news/".$item->id."/660x330/mdpi/".$item->image ;
 
        }
-}
+
+       $item->created_at = Carbon::parse($item->created_at)->format('d.m.Y');
+});
 
                 return $articles;
         }
