740d99948ad44204a73a03cda4d5b9fdb5ae3717
Karam Henni <karam.tenes@gmail.com>
2020-08-18 12:18:34 +0100

    fixing header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -33,43 +33,20 @@
 
                     @endforeach
                                                             
-                                                        </ul>
+               </ul>
             <div class="tab-content">
-              @foreach($categories->take(5) as $key => $category)
-              @if($key == 0) 
+              
                 <div class="tab-pane show in active fade " id="{{$category->slug}}" role="tabpanel">
-              @else
-                <div class="tab-pane fade " id="{{$category->slug}}" role="tabpanel">
-              @endif
+        
                 <div class="row">
-                  @foreach($category->articles->take(4) as $article)
-                 <div class="col-lg-6 col-md-6">
-                    <div class="tw1_list clearfix">
-                            <div class="tw1l_thumb" style="width:155px; height:90px;">
-                                   <img loading="lazy" src="{{$article->getAvatar(155,90)}}" alt="">
-                                        <div class="tw1l_cats">
-                                        <a href="{{Route('categories.show',$article->Category->slug)}}" class="cats reds">{{$article->Category->nom}}</a>
-                                                                                </div>
-                                                                            </div>
-                                <div class="tw1_l_content">
-                                    
-                                        <h3><a href="/news/{{$article->tag}}">{{$article->titre}}</a></h3>
-                                   
-                                    <div class="comon_meta clearfix">
-                                <span class="cm_author"><i class="feather icon-user"></i>By<a href="{{Route('editors.show',$article->Creator->slug)}}">{{$article->Creator->Full_Name}}</a></span>
-                            <span class="cm_date"><a href="#">{{$article->DateActu}}</a></span>
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
-                 </div>
-                 @endforeach
+                    Tab
                 </div>
                 </div>
-                     @endforeach
 
-                                                        </div>
-                                                    </div>
-                                                </div>
+
+             </div>
+        </div>
+        </div>
                                             </li>
                                             <li class="menu-item-has-children">
                                                 <a href="#">Gallery</a>
