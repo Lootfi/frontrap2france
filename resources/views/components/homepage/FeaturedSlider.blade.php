b410592de9005375c590ff8b5ca91d8ff517e91b
Karam Henni <karam.tenes@gmail.com>
2020-08-22 21:25:21 +0100

    fix responsive owl

--- resources/views/components/homepage/FeaturedSlider.blade.php
+++ resources/views/components/homepage/FeaturedSlider.blade.php
@@ -16,7 +16,7 @@
                                             <a href="/news/{{$article->tag}}" class="read_more">Read More</a>
                                         </div>
                                     </div>
-                                    <div class="col-lg-6 col-md-6 noPaddingRight">
+                                    <div class="col-lg-5 col-md-5 noPaddingRight">
                                         <div class="feature_img">
                                             <img loading="lazy"  src="{{$article->getAvatar(290,380)}}" alt="">
                                         </div>
